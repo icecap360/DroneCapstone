e49b1ee1ceb8db6a0e224ee4e2042ce6850435a5
Muhammad Ali <alim102@mcmaster.ca>
2023-02-04 00:58:23 -0500

    Got map to work

--- src/OperatorApp/Main.py
+++ src/OperatorApp/Main.py
@@ -3,14 +3,14 @@ from PyQt5.QtWidgets import QApplication, QWidget
 import sys
 import UserInterface #import the setup code for the app
 
-class DroneApp(QtWidgets.QMainWindow, UserInterface.Ui_MainWindow1): #the method for the main app
+class DroneApp(QtWidgets.QMainWindow, UserInterface.UIController): #the method for the main app
     def __init__(self, parent=None):
         super(DroneApp, self).__init__(parent)
         self.setupUi(self)
         self.camocc = CamOccApp()
         self.camocc.show()
         
-class CamOccApp(QtWidgets.QMainWindow, UserInterface.Ui_MainWindow2): #the method for the sub app
+class CamOccApp(QtWidgets.QMainWindow, UserInterface.UIDisplayer): #the method for the sub app
     def __init__(self, parent=None, child=None):
         super().__init__()
         self.setupUi(self)
