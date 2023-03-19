d4ac03d704d20b4244e4c1c3f0933a4773ec9ec6
Winnie Trandinh <22330162+WinnieTrandinh@users.noreply.github.com>
2023-03-19 13:59:57 -0400

    Merged in commit 60fc210 from Computer Vision. Retuned segmenter and refactored into VisionApp class.
    Bounding contour merged from commit. Created new VisionAppPC class to handle parking lot bounds segmentation.

--- test/ComputerVision/AlgorithmWinnie.py
+++ test/ComputerVision/AlgorithmWinnie.py
@@ -1,49 +1,147 @@
-import numpy as np
 import cv2 as cv
-from matplotlib import pyplot as plt
-from PIL import Image
+import numpy as np
+from pdb import set_trace as bp
+
+# debug levels (0 to 3)
+debug = 2
+
+def thresholdGray(img, maxSaturation):
+	img_hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+	#gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+	#ret, threshRGB = cv.threshold(gray, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)
+	threshHSV = cv.inRange(img_hsv, (0, 0, 0), (180, maxSaturation, 255))
+
+	# close original image to remove noise
+	closedImg = cv.morphologyEx(threshHSV, cv.MORPH_CLOSE, np.ones((3,3), np.uint8), iterations = 3)
+
+	return threshHSV, closedImg
+
+def applyWatershed(origImg, threshImg):
+	# noise removal
+	kernel = np.ones((3,3),np.uint8)
+	kernel5 = np.ones((6,6), np.uint8)
+	opening = cv.morphologyEx(threshImg,cv.MORPH_OPEN,kernel5, iterations = 2)
+	# sure background area
+	sure_bg = cv.dilate(opening,np.ones((12,12), np.uint8), iterations=4)
+	# Finding sure foreground area
+	#dist_transform = cv.distanceTransform(opening,cv.DIST_L2,5)
+	#ret, sure_fg = cv.threshold(dist_transform,0.7*dist_transform.max(),255,0)
+	closing = cv.morphologyEx(threshImg,cv.MORPH_CLOSE,kernel5, iterations = 2)
+	sure_fg = cv.erode(closing,np.ones((9,9), np.uint8),iterations=4)
+	# Finding unknown region
+	sure_fg = np.uint8(sure_fg)
+	unknown = cv.subtract(sure_bg,sure_fg)
+
+	if debug >= 2:
+		cv.imshow('bg', sure_bg)
+		cv.imshow('fg', sure_fg)
+
+	# Marker labelling
+	ret, markers = cv.connectedComponents(sure_fg)
+	# Add one to all labels so that sure background is not 0, but 1
+	markers = markers+1
+	# Now, mark the region of unknown with zero
+	markers[unknown==255] = 0
+	markers = cv.watershed(origImg, markers)
+	origImg[markers == -1] = [255,0,0]
+
+	return img, markers
+
+def calcWhiteAreaInContour(contour, threshImg):
+	# check all pixels if inside contour
+	# if inside, check corresponding pixel on original thresholded image
+	# find combined area of white blobs (parking lot) within original thresholded image, bounded by contour
+	whiteArea = 0
+	for i in range(0, threshImg.shape[0]):
+		for j in range(0, threshImg.shape[1]):
+			# (x,y)
+			pt = (j, i)
+			# returns 1 if inside, 0 if on contour, -1 if outside
+			within = cv.pointPolygonTest(contour, pt, False)
+			if within == 0 or within == 1:
+				whiteArea += threshImg[i,j]/255
+
+	# calc percetange area of contour that is white (parking lot)
+	return whiteArea / cv.contourArea(contour)
+
+def findLargestParkingContour(markers, threshImg, areaThreshold):
+	# contouring
+	#binaryImg = threshHSV;
+	binaryImg = np.zeros((threshImg.shape[0], threshImg.shape[1], 1), np.uint8)
+	binaryImg[markers == -1] = 255
+	contours, hier = cv.findContours(binaryImg, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+	cv.drawContours(binaryImg, contours, -1, 150, 1)
+
+	# get largest contour
+	maxArea = 0
+	maxContour = contours[0]
+	for cnt in contours:
+		area = cv.contourArea(cnt)
+		#if area == 11410:
+		#	bp()
+		if area == ( (binaryImg.shape[0]-1) * (binaryImg.shape[1]-1) ):
+			# contour is around the whole image
+			continue
+		# checks if contour is surrounding parking lot area or non-parking lot area
+		if area > maxArea:
+			percentageArea = calcWhiteAreaInContour(cnt, threshImg)
+			if debug >= 2:
+				print("Percentage area = " + str(percentageArea))
+			if (percentageArea >= areaThreshold):
+				maxArea = area
+				maxContour = cnt
+		# debugging for displaying all contours
+		if debug == 3:
+			tempImg = np.zeros((threshImg.shape[0], threshImg.shape[1], 1), np.uint8)
+			cv.drawContours(tempImg, [cnt], 0, 255, 3)
+			cv.imshow("currentContour", tempImg)
+			cv.waitKey(0)
+
+	if debug:
+		cv.drawContours(binaryImg, [maxContour], 0, 255, 3)
+		cv.imshow("maxContour", binaryImg)
+
+	return maxContour
 
 
-img = cv.imread('street_image.png', cv.IMREAD_ANYCOLOR)
-# Convert RGB to HSV
-hsv = cv.cvtColor(img, cv.COLOR_RGB2HSV)
-# define range of gray color in HSV
-lower_gray = np.array([0, 0, 0])
-upper_gray = np.array([255, 25, 255])
-# Threshold the HSV image to get only gray colors
-thresh = cv.inRange(hsv, lower_gray, upper_gray)
-cv.imshow("thresholding", thresh)
+if __name__ == '__main__':
+	# Hue = [0, 180]
+	# Saturation = [0, 255]
+	# Value = [0, 255]
+	maxSaturation = 35		# 30
+	areaThreshold = 0.6
 
+	#for i in range(0, 30):
+	for i in {5}:
+		img = cv.imread("SattaliteImagery\\TestSet\\ParkingLot" + str(i).zfill(2) + ".png")
 
+		# bad images = 8, 9, 12, 13, 20, 21, 23, 24, 25, 26, 27, 28, 29
+		# FP = 00, 05
 
-#gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
-#ret, thresh = cv.threshold(gray,0,255,cv.THRESH_BINARY_INV+cv.THRESH_OTSU)
-#print(ret)
+		# thresholds by HSV values to extract gray regions
+		# closedImg features an additional Closing operation to remove noise
+		threshHSV, closedImg = thresholdGray(img, maxSaturation)
 
-# noise removal
-kernel = np.ones((3,3),np.uint8)
-opening = cv.morphologyEx(thresh,cv.MORPH_OPEN,kernel, iterations = 2)
-# sure background area
-sure_bg = cv.dilate(opening,kernel,iterations=3)
-closing = cv.morphologyEx(thresh,cv.MORPH_CLOSE,kernel, iterations = 2)
-# Finding sure foreground area
-#dist_transform = cv.distanceTransform(opening,cv.DIST_L2,5)
-#ret, sure_fg = cv.threshold(dist_transform,0.7*dist_transform.max(),255,0)
-sure_fg = cv.erode(closing,kernel,iterations=3)
+		if debug:
+			cv.imshow('hsvThresh', threshHSV)
+			cv.imshow("close", closedImg)
 
-# Finding unknown region
-sure_fg = np.uint8(sure_fg)
-unknown = cv.subtract(sure_bg,sure_fg)
+		# applies watershed segmentation to get boundaries
+		img, markers = applyWatershed(img, closedImg)
 
-# Marker labelling
-ret, markers = cv.connectedComponents(sure_fg)
-# Add one to all labels so that sure background is not 0, but 1
-markers = markers+1
-# Now, mark the region of unknown with zero
-markers[unknown==255] = 0
+		if debug:
+			cv.imshow("imgShed", img)
 
-markers = cv.watershed(img,markers)
-img[markers == -1] = [255,0,0]
+		# finds the largest contour of the parking lot
+		maxContour = findLargestParkingContour(markers, closedImg, areaThreshold)
 
+		if debug:
+			threshHSV_rgb = cv.cvtColor(threshHSV, cv.COLOR_GRAY2BGR)
+			cv.drawContours(threshHSV_rgb, [maxContour], 0, (0, 255, 0), 3)
+			cv.imshow("hsvThresh", threshHSV_rgb);
 
-cv.imshow("pic", img)
+		# output display
+		cv.drawContours(img, [maxContour], 0, (0, 255, 0), 3)
+		cv.imshow("parkinglotSeg", img)
+
+		cv.waitKey(0)
--- test/ComputerVision/VisionApp.py
+++ test/ComputerVision/VisionApp.py
@@ -1,25 +1,57 @@
 import numpy as np
-import cv2 
+import cv2
 from matplotlib import pyplot as plt
 from PIL import Image
 from abc import ABC
-from Segmenters import ParkingLotSegmenter, NatureSegmenter
+from Segmenters import ParkingLotSegmenter, NatureSegmenter, ParkingLotBounds
 from DataSetManager import DataSetManager
 from Classifier import Classifier
 class VisionAppPi:
-    def __init__(self) -> None:
-        self.parkLotSegmenter = ParkingLotSegmenter()
-        self.natureSegmenter = NatureSegmenter()
-        self.classifier = Classifier()
-        self.parkLotDetected = None
-        self.natureDetected = None
-        self.occupiedDetected = None
-    def processImage(self, img, cropImage=True, exeParkLot=True, exeNature=True):
-        if exeParkLot:
-            self.parkLotSegmenter.process(img, cropImage)
-            self.parkLotDetected  = self.classifier.classify(self.parkLotSegmenter.getResult())
-        if exeNature:
-            self.natureSegmenter.process(img, cropImage)
-            self.natureDetected  = self.classifier.classify(self.natureSegmenter.getResult())
-        if exeNature and exeParkLot:
-            self.occupiedDetected = (not self.natureDetected) and (not self.parkLotDetected)
+	def __init__(self) -> None:
+
+		kernels = {
+			"Open": np.ones((3,3), np.uint8),
+			"Close": np.ones((3,3), np.uint8),
+			"Dilate": np.ones((3,3), np.uint8),
+			"Erode": np.ones((3,3), np.uint8)
+		}
+		self.parkLotSegmenter = ParkingLotSegmenter(kernels)
+		self.natureSegmenter = NatureSegmenter(kernels)
+		self.classifier = Classifier()
+		self.parkLotDetected = None
+		self.natureDetected = None
+		self.occupiedDetected = None
+	def processImage(self, img, cropImage=True, exeParkLot=True, exeNature=True):
+		if exeParkLot:
+			self.parkLotSegmenter.process(img, cropImage)
+			self.parkLotDetected  = self.classifier.classify(self.parkLotSegmenter.getResult())
+		if exeNature:
+			self.natureSegmenter.process(img, cropImage)
+			self.natureDetected  = self.classifier.classify(self.natureSegmenter.getResult())
+		if exeNature and exeParkLot:
+			self.occupiedDetected = (not self.natureDetected) and (not self.parkLotDetected)
+
+
+class VisionAppPC:
+	def __init__(self) -> None:
+
+		kernels = {
+			"Open": np.ones((6,6), np.uint8),
+			"Close": np.ones((6,6), np.uint8),
+			"Dilate": np.ones((12,12), np.uint8),
+			"Erode": np.ones((9,9), np.uint8)
+		}
+		self.parkLotBounds = ParkingLotBounds(kernels, maxSaturation=35, areaThreshold=0.6)
+		self.maxContour = None
+
+	def processImage(self, img):
+		self.parkLotBounds.process(img)
+		self.maxContour = self.parkLotBounds.getContour()
+
+
+# if __name__ == '__main__':
+# 	visionAppPC = VisionAppPC()
+#
+# 	for i in range(0,30):
+# 		img = cv2.imread("SattaliteImagery\\TestSet\\ParkingLot" + str(i).zfill(2) + ".png")
+# 		visionAppPC.processImage(img)
