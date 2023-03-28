5f37c1b22874eca9e82fdaa03c2ed0de901fb380
Muhammad Ali <icecap360@live.com>
2023-03-29 00:12:24 +0100

    MainPI Algo_app

--- src/DroneApp/src/algo_app/src/MainPI.py
+++ src/DroneApp/src/algo_app/src/MainPI.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-import sys
+import sys, time
 sys.path.append('/home/pi/DroneCapstone/src')
 import rospy
 from std_msgs.msg import String
@@ -9,11 +9,12 @@ from MapperApp import MapperApp
 from PathPlanApp import PathPlanApp
 from Utils import DroneCamera
 from TopicInterface import TopicInterface
+from threading import Event
 
 if __name__ == "__main__":
-    rospy.init_node('vision_app', anonymous=True)
+    rospy.init_node('algo_app', anonymous=True)
     rate = rospy.Rate(10)
-
+    stopNodeEvent = Event()
     algorithmApp = AlgorithmApp()
     visionApp = VisionApp()
     mapperApp = MapperApp()
@@ -22,14 +23,7 @@ if __name__ == "__main__":
     topicInterface = TopicInterface()
     algorithmApp.init(visionApp, mapperApp,pathPlanApp, droneCamera, topicInterface )
 
-    #fly in circle testcase
-    #stateMachine.TestCircularMotion()
-
-    #test launch 
-    try:
-        while True:
-            algorithmApp.process()
-            rate.sleep()
-            #algorithmApp.rate.sleep(1)
-    except KeyboardInterrupt:
-        print('Keyboard expection')
+    while not rospy.is_shutdown():
+        algorithmApp.process()
+        rate.sleep()
+        time.sleep(1)
